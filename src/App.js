929edb46f0f120912958d7c578b62d77a2c8ff2b
Andrew <andrew@coding>
2024-05-04 19:15:23 -0400

    Example of a state added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 function App() {
+    const [count, setCount] = useState(0);
+
     const handleClick = () => {
-        console.log("Button was clicked");
+        setCount(count+1);
     };
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
+            <div>
+                <div>Number of animals is {count}</div>
+            </div>
         </div>
     );
 }
